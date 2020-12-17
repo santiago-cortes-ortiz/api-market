b94e2823c74e1f617b4751b66876c6559546e534
santiago-cortes-ortiz <jeissoncortes02@outlook.com>
2020-12-17 12:58:10 -0600

    se mapearon las entidades de la base de datos en clases java para la aplicacion. Ademas se adicionaron los atributos en dichas clases.

---
+++ src/main/java/com/jeiss/market/persistence/entity/Compra.java
@@ -0,0 +1,75 @@
+package com.jeiss.market.persistence.entity;
+
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "compras")
+public class Compra {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_compra")
+    private Integer idCompra;
+
+    @Column(name = "id_cliente")
+    private Integer idCliente;
+
+    private Date fecha;
+
+    @Column(name = "medio_pago")
+    private Character medioPago;
+
+    private String comentario;
+
+    private Boolean estado;
+
+    public Integer getIdCompra() {
+        return idCompra;
+    }
+
+    public void setIdCompra(Integer idCompra) {
+        this.idCompra = idCompra;
+    }
+
+    public Integer getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(Integer idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Character getMedioPago() {
+        return medioPago;
+    }
+
+    public void setMedioPago(Character medioPago) {
+        this.medioPago = medioPago;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+}
