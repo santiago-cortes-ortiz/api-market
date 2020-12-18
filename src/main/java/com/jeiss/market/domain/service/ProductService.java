85640e1dbcd303ce7e8b63b2f7333eebd351b0ab
santiago-cortes-ortiz <jeissoncortes02@outlook.com>
2020-12-18 17:38:07 -0600

    Se creo una clase ServiceProduct y se adicionaron los metodos que se ultilizaran en el srvicio con la anotacion @Service

---
+++ src/main/java/com/jeiss/market/domain/service/ProductService.java
@@ -0,0 +1,47 @@
+package com.jeiss.market.domain.service;
+
+import com.jeiss.market.domain.Product;
+import com.jeiss.market.domain.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> getAll(){
+        return productRepository.getAll();
+    }
+
+    public Optional<Product> getProduct(int productId){
+        return productRepository.getProduct(productId);
+    }
+
+    public Optional<List<Product>> getByCategory(int categoryId){
+        return productRepository.getByCategory(categoryId);
+    }
+
+    public Product save(Product product){
+        return productRepository.save(product);
+    }
+
+    public boolean delete(int productId){
+        /*return getProduct(productId)
+                .map(product -> {
+                    productRepository.deleteProduct(productId);
+                    return true;
+                }).orElse(false);*/
+        try {
+            productRepository.delete(productId);
+            return true;
+        } catch (EmptyResultDataAccessException e) {
+            return false;
+        }
+    }
+}
--- src/main/java/com/jeiss/market/persistence/ProductoRepository.java
+++ src/main/java/com/jeiss/market/persistence/ProductoRepository.java
@@ -5,6 +5,7 @@ import com.jeiss.market.domain.repository.ProductRepository;
 import com.jeiss.market.persistence.crud.ProductoCrudRepository;
 import com.jeiss.market.persistence.entity.Producto;
 import com.jeiss.market.persistence.mapper.ProductMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,7 +14,10 @@ import java.util.Optional;
 @Repository
 public class ProductoRepository implements ProductRepository {
 
+    @Autowired
     private ProductoCrudRepository productoCrudRepository;
+
+    @Autowired
     private ProductMapper productMapper;
 
     @Override
