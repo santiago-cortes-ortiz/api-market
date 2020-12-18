dea1b62424671ebd3ed38c11af20a9413720abb1
santiago-cortes-ortiz <jeissoncortes02@outlook.com>
2020-12-18 10:28:27 -0600

    Se implemento lo que se llama el mapper data en el cual logramos no exponer nuestra base de datos en la api, para esto contruimos clases en la cula hacen una misma labor.

---
+++ src/main/java/com/jeiss/market/domain/repository/ProductRepository.java
@@ -0,0 +1,16 @@
+package com.jeiss.market.domain.repository;
+
+import com.jeiss.market.domain.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository {
+    List<Product> getAll();
+    Optional<List<Product>> getByCategory(int categoryId);
+    Optional<List<Product>> getScarseProducts(int quantity);
+    Optional<List<Product>> getLowerPriceSale(int priceSale, int categoryId);
+    Optional<Product> getProduct(int productId);
+    Product save(Product product);
+    void delete(int productId);
+}
---
+++ src/main/java/com/jeiss/market/persistence/mapper/CategoryMapper.java
@@ -0,0 +1,5 @@
+package com.jeiss.market.persistence.mapper;
+
+public interface CategoryMapper {
+
+}
