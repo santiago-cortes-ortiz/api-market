cfa6150f07d1883252346ce9f51418605a656b07
santiago-cortes-ortiz <jeissoncortes02@outlook.com>
2021-02-09 14:13:09 -0600

    Se adicionaron metodos para poder hacer autorizaciones sobre tokens

--- src/main/java/com/jeiss/market/domain/service/MarketUserDetailsService.java
+++ src/main/java/com/jeiss/market/domain/service/MarketUserDetailsService.java
@@ -15,4 +15,25 @@ public class MarketUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return new User("jeisson","{noop}market", new ArrayList<>());
     }
+
+
+    /**
+     * private static List<User> users = new ArrayList();
+     *
+     *     public MarketUserDetailsService() {
+     *         users.add(new User("jeisson", "{noop}market", new ArrayList<>()));
+     *     }
+     *
+     *     @Override
+     *     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+     *         // TODO ir a BD por usuarios o sistemas AUTH0
+     *         Optional<User> user = users.stream().filter(u -> u.getUsername().equals(username)).findAny();
+     *         if (!user.isPresent()) {
+     *             thrownew UsernameNotFoundException("User not found by name: " + username);
+     *         }
+     *         returnnew User(user.get().getUsername(), user.get().getPassword(), user.get().getAuthorities());
+     *     }
+     *
+     *
+     * **/
 }
--- src/main/java/com/jeiss/market/web/security/JWTUtil.java
+++ src/main/java/com/jeiss/market/web/security/JWTUtil.java
@@ -1,5 +1,6 @@
 package com.jeiss.market.web.security;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,5 +18,22 @@ public class JWTUtil {
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
                 .signWith(SignatureAlgorithm.HS256,SECRETKEY)
                 .compact();
+    }
+
+    public boolean validateToken(String token, UserDetails user){
+
+        return user.getUsername().equals(extractUsername(token)) && !isTokenExpired(token);
+    }
+
+    public String extractUsername(String token){
+        return getClaims(token).getSubject();
+    }
+
+    public boolean isTokenExpired(String token){
+        return getClaims(token).getExpiration().before(new Date());
+    }
+
+    private Claims getClaims(String token){
+        return Jwts.parser().setSigningKey(SECRETKEY).parseClaimsJws(token).getBody();
     }
 }
