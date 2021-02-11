e77540aed4587bccbcbcba957a1229b445d74f7d
santiago-cortes-ortiz <jeissoncortes02@outlook.com>
2021-02-10 18:06:14 -0600

    Se finalizo el apartado de seguridad con jwt y se adiciono una clase encargada de hacer el filtro y autorizar al usuario en consumir nuestra api

--- src/main/java/com/jeiss/market/web/security/SecurityConfig.java
+++ src/main/java/com/jeiss/market/web/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.jeiss.market.web.security;
 
 import com.jeiss.market.domain.service.MarketUserDetailsService;
+import com.jeiss.market.web.security.filter.JwtFilterRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -9,12 +10,17 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private MarketUserDetailsService marketUserDetailsService;
+
+    @Autowired
+    private JwtFilterRequest jwtFilterRequest;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -24,8 +30,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests().antMatchers("/**/authenticate").permitAll()
-        .anyRequest().authenticated();
+        .anyRequest().authenticated().and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
+
+        http.addFilterBefore(jwtFilterRequest, UsernamePasswordAuthenticationFilter.class);
     }
 
     @Override
